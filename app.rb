7ef2090c6f2d10658bff5855a2965778e62c8635
Vicky Gray <vickygray26@hotmail.com>
2016-02-01 19:26:40 +0000

    added pages

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
+# set :session_secret, 'super secret'
 require 'sinatra'
+
 
 get '/' do
   "Hello, world!"
 end
 
-get'/secret' do
-"I'm over here..."
+get '/secret' do
+  "I'm over here..."
+end
+
+get '/next' do
+  "This is another page!"
+end
+
+get '/cat' do
+  "<div style='border-width: 2px; border-style: dashed; border-color: red;'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
