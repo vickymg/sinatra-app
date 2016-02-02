394f1ee4b67bab04da8f8a515d1a5d5ddbf62f3b
Makers Academy <makers@Makerss-MacBook-Pro-2.local>
2016-02-02 11:50:41 +0000

    Added params

--- app.rb
+++ app.rb
@@ -23,7 +23,13 @@ get '/groundhog' do
   "groundhog"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
